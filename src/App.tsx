6835138c9e697af18c4274c9b80ca6b888428235
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-17 13:27:45 -0600

    add basic form boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Form from './components/form/Form';
 
 function App() {
-  return <h1>Yo</h1>;
+  return (
+    <>
+      <Form />
+    </>
+  );
 }
 
 export default App;
