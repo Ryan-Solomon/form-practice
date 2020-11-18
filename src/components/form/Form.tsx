f13145b21e6842f22b87c1902bb69c2bf0827f70
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-17 21:12:14 -0600

    add form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,54 +1,48 @@
 import React from 'react';
+import './Form.styles.scss';
 
 function Form() {
   return (
     <main className='outer-container'>
-      <section className='form-container'>
-        <form>
-          <header>
-            <h2>Sign Up</h2>
-          </header>
-          <div className='input first-name'>
-            <label htmlFor='first-name'>First Name: </label>
-            <input
-              id='first-name'
-              type='text'
-              placeholder='First Name'
-              required
-              autoFocus
-            />
-          </div>
-          <div className='input last-name'>
-            <label htmlFor='last-name'>Last Name: </label>
-            <input
-              id='last-name'
-              type='text'
-              placeholder='Last Name'
-              required
-            />
-          </div>
-          <div className='input email'>
-            <label htmlFor='email'>Email: </label>
-            <input id='email' type='email' placeholder='Email' required />
-          </div>
-          <div className='input password'>
-            <label htmlFor='password'>Password: </label>
-            <input
-              id='password'
-              type='password'
-              placeholder='Password'
-              required
-            />
-          </div>
-          <div className='sign-up btn'>
-            <button>Sign Up</button>
-          </div>
-          <footer>
-            <h4>By clicking you agree to</h4>
-            <h4>Terms and Conditions</h4>
-          </footer>
-        </form>
-      </section>
+      <form className='form-container'>
+        <header>
+          <h2>Sign Up</h2>
+        </header>
+        <div className='input first-name'>
+          <label htmlFor='first-name'>First Name </label>
+          <input
+            id='first-name'
+            type='text'
+            placeholder='First Name'
+            required
+            autoFocus
+          />
+        </div>
+        <div className='input last-name'>
+          <label htmlFor='last-name'>Last Name </label>
+          <input id='last-name' type='text' placeholder='Last Name' required />
+        </div>
+        <div className='input email'>
+          <label htmlFor='email'>Email </label>
+          <input id='email' type='email' placeholder='Email' required />
+        </div>
+        <div className='input password'>
+          <label htmlFor='password'>Password </label>
+          <input
+            id='password'
+            type='password'
+            placeholder='Password'
+            required
+          />
+        </div>
+        <div className='sign-up btn'>
+          <button>Sign Up</button>
+        </div>
+        <footer>
+          <h4>By clicking you agree to</h4>
+          <h4>Terms and Conditions</h4>
+        </footer>
+      </form>
     </main>
   );
 }
